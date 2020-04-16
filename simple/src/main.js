eefaf210644c55ca38e1c5c60ca2a5fab1ea1d87
“jibingkun1 <j837351979@163.com>
2020-04-16 23:04:40 +0800

    4/16练习

---
+++ simple/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Layout from './Layout.vue'
+import IndexPage from './pages/index.vue'
+import mock from './mock/mock'
+
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:IndexPage
+    }
+  ]
+})
+
+new Vue({
+  el: '#app',
+  router,
+  components:{
+    Layout
+  },
+  template: '<Layout/>'
+})
